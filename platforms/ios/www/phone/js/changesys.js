240a5091cdca55ee80f516c65e471b8743c3ddb2
PepperYan <hibad@zhenjianpengtekiMacBook-Pro.local>
2013-11-08 16:14:15 +0800

    login finish

--- platforms/ios/www/phone/js/changesys.js
+++ platforms/ios/www/phone/js/changesys.js
@@ -1,56 +1,74 @@
 var me;
 var changesys = {
-initialize:function(){
-    me = this;
-    document.addEventListener('deviceready', this.onDeviceReady, false);
-},
-onDeviceReady:function(){
-    // this.getSysInfo(this.bindEvents);
-    me.bindEvents();
-},
-bindEvents:function(){
-    $("#title").bind("click",function(){
-                     $("#sysSelect").focus();
-                     })
-    
-    $("#sysSelect").bind("change",function(){
-        me.showLoginView(this.value);
-    })
-},
-disableEvents:function(){
-    $("#title").unbind("click");
-    $("#sysSelect").unbind("change");
-},
-getSysInfo:function(callback){
-    if(this.hasCordova()){
-        callback();
-    }
-},
-hasCordova:function(){
-    if(cordova && cordova.exec){
-        return true;
-    }else{
-        alert("请检查phonegap配置");
-        return false;
-    }
-},
-showLoginView:function(title){
-    $("#change_sys_login_title").html("登陆"+title);
+    initialize:function(){
+        me = this;
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady:function(){
+        // this.getSysInfo(this.bindEvents);
+        me.bindEvents();
+    },
+    bindEvents:function(){
+        $("#title").bind("click",function(){
+                         $("#sysSelect").focus();
+                         })
+        
+        $("#sysSelect").bind("change",function(){
+            me.showLoginView(this.value);
+        })
+    },
+    disableEvents:function(){
+        $("#title").unbind("click");
+        $("#sysSelect").unbind("change");
+    },
+    getSysInfo:function(callback){
+        if(this.hasCordova()){
+            callback();
+        }
+    },
+    hasCordova:function(){
+        if(cordova && cordova.exec){
+            return true;
+        }else{
+            alert("请检查phonegap配置");
+            return false;
+        }
+    },
+    //显示登陆框
+    showLoginView:function(title){
+        $("#change_sys_login_title").html("登陆"+title);
 
-    // $("#username").click();
+        // $("#username").click();
 
-    $("#change_sys_login_wrapper").show();
-    $("#change_sys_submit").unbind("click");
-    $("#change_sys_submit").bind("click",function(){
-        console.log("提交登陆信息")
-    });
-    $("#change_sys_cancel").unbind("click");
-    $("#change_sys_cancel").bind("click",function(){
-         console.log("关闭窗口");
-         $("#change_sys_login_wrapper").find("input").val("");
-         $("#change_sys_login_wrapper").hide();
-    })
-}
+        $("#change_sys_login_wrapper").show();
+        $("#change_sys_submit").unbind("click");
+        $("#change_sys_submit").bind("click",function(){
+            console.log("提交登陆信息");
+            $("#change_sys_submit").html("取消");
+            me.onLoginSuccess();
+        });
+        $("#change_sys_cancel").unbind("click");
+        $("#change_sys_cancel").bind("click",function(){
+             console.log("关闭窗口");
+             $("#change_sys_login_wrapper").find("input").val("");
+             $("#wrapper").click();
+             $("#change_sys_login_wrapper").hide();
+        })
+    },
+    //登陆成功
+    onLoginSuccess:function(){
+       var sysName = $("#sysSelect").val(); 
+       $("#title").html(sysName);
+       $("#change_sys_login_wrapper").hide();
+    },
+    //取消登陆
+    onLoginCanceled:function(){
+
+    },
+    //登陆失败
+    onLoginFail:function(){
+        
+    }
 }
 
 changesys.initialize();
