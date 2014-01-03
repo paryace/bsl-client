20567dece83ceafaa87cdea80e6173922845bdf5
PepperYan <188266760@qq.com>
2014-01-03 17:07:03 +0800

    login

--- platforms/ios/www/phone/js/login.js
+++ platforms/ios/www/phone/js/login.js
@@ -72,13 +72,14 @@ $("#LoginBtn").click(function() {
     if(isRemember === 'false')
     {
         $("#realpsw").val("");
-
+        $("#password").val("");
     }
 	//是否启用离线登陆
 	var isOffLine = $('#isOffLine:checked').val();
 	if(isOffLine === undefined){
 		isOffLine = "false";
 	}
+	alert("un->"+username+",psw->"+password);
 	cordova.exec(function(data) {
 		data = $.parseJSON(data);
 		if (data.isSuccess === true) {
